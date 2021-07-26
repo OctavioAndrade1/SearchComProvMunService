c644cb8d34c731232c5286a3c6f571dc7228eb96
OctavioAndrade <octavio.andrade.17@gmail.com>
2021-07-26 16:44:46 +0100

    hello
    project

---
+++ SearchComProvMun/SearchComProvMun/src/test/java/com/teamc/SearchComProvMun/SearchComProvMunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamc.SearchComProvMun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchComProvMunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
