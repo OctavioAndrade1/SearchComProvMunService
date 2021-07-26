4580c2011c96a08d761b127fea33d0dbb76a4d31
OctavioAndrade1 <87991449+OctavioAndrade1@users.noreply.github.com>
2021-07-26 18:00:36 +0100

    Add files via upload

---
+++ SearchComProvMun/src/main/java/com/teamc/SearchComProvMun/SearchComProvMunApplication.java
@@ -0,0 +1,13 @@
+package com.teamc.SearchComProvMun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchComProvMunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchComProvMunApplication.class, args);
+	}
+
+}
